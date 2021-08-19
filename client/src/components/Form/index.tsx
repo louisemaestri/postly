a90a3133279aba13b593f5bbee72b72dcddc85fb
Louise Maestri <louisemaestri1@gmail.com>
2021-08-19 19:19:56 -0300

    Add a main file to export all form components

---
+++ client/src/components/Form/index.tsx
@@ -0,0 +1,4 @@
+export { default as TextField } from './TextField';
+export { default as TextAreaField } from './TextAreaField';
+export { default as CheckboxField } from './CheckboxField';
+export { default as FieldGroup } from './FieldGroup';
